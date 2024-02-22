63600f793a3258fc850f6d1f53b2df09bbb08159
Rob Chiocchio <rmchiocchio@wpi.edu>
2024-02-22 09:45:50 +0100

    iVWAP/Bogus

--- eval.py
+++ eval.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def calculate_vwap(price, volume):
+def calculate_institutional_vwap(price, volume):
     """
-    Calculate the Volume Weighted Average Price (VWAP)
+    Calculate the institutional Volume Weighted Average Price (VWAP)
 
     Parameters:
         price (array): The price of the asset
@@ -14,4 +14,21 @@ def calculate_vwap(price, volume):
     """
     vwap_values = np.cumsum(price * volume) / np.cumsum(volume)
     final_vwap = vwap_values[-1]
+    return final_vwap, vwap_values
+
+def calculate_bogus_vwap(price, volume):
+    """
+    Calculate the bogus Volume Weighted Average Price (VWAP)
+
+    Parameters:
+        price (array): The price of the asset
+        volume (array): The volume of the asset
+
+    Returns:
+        final_vwap (float): The vwap of the entire period
+        vwap_values (array): The vwap values at each point in time
+    """
+    #vwap_values = (price * volume) / (volume)
+    vwap_values = price # assuming price is the day's vwap
+    final_vwap = vwap_values.mean()
     return final_vwap, vwap_values
